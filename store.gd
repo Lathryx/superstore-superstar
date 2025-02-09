23140615450a4b358e5bf1b5d720cb33c3da7654
Lathryx <52394238+Lathryx@users.noreply.github.com>
2025-02-09 06:52:06 -0500

    Senile spawning

--- store.gd
+++ store.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var senile_scene: PackedScene 
+
 var used_rect 
 
 func _ready():
@@ -9,3 +11,15 @@ func _ready():
 	$Player/Camera2D.limit_left = 0 
 	$Player/Camera2D.limit_bottom = used_rect.y*16 
 	$Player/Camera2D.limit_right = used_rect.x*16 
+	$SenileTimer.start() 
+	
+func _on_senile_timer_timeout(): 
+	var senile = senile_scene.instantiate() 
+	
+	var senile_spawn_location = $SenilePath/SenileSpawnLocation 
+	senile_spawn_location.progress_ratio = randf() 
+	
+	senile.position = senile_spawn_location.position 
+	
+	add_child(senile) 
+	print("added senile") 
