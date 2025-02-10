85f7344676d54167572b012062b5e5e6061297eb
Saidan816 <162340695+Saidan816@users.noreply.github.com>
2025-02-10 15:27:34 -0500

    Music note no longer blurry
    Changed music note from process to physics process

--- Scripts/music_note.gd
+++ Scripts/music_note.gd
@@ -7,14 +7,15 @@ func _ready() -> void:
 	var num_types = 3 
 	var path = "res://Resources/musicnote%d.tres" % (randi() % num_types + 1)
 	$AnimatedSprite2D.sprite_frames = load(path) 
-	$AnimatedSprite2D.play("default")
+	$AnimatedSprite2D.play()
 	#print("hello")
 	#print($AnimatedSprite2D.sprite_frames) 
 
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position()) 
+	position += direction * speed * delta 
 	
 
 func _process(delta): 
-	position += direction * speed * delta 
+	pass
