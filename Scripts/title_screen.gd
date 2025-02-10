73d6202f0c7e3324610cc5a8c166aca51a3b34f2
Saidan816 <162340695+Saidan816@users.noreply.github.com>
2025-02-09 20:05:40 -0500

    Hewo i added da title screen
    button is invisible located on the Start Game letters, because default button is ugly.

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/store.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
